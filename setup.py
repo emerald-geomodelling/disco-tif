6e5a0897deb18550bbb63b2ad5c35017a59610fd
Benjamin R. Bloss <bloss.benjamin@gmail.com>
2024-04-17 09:46:50 +0200

    added a history.md and a setup.py file

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os,sys
+from setuptools import setup, find_packages
+
+git_token = os.environ["GITHUB_TOKEN_EMRLD"]
+if not "GITHUB_TOKEN_EMRLD" in os.environ:
+    print("Github token (GITHUB_TOKEN_EMRLD) not set!", file=sys.stderr)
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+requirements = ["seaborn",
+                "rasterio",
+                "numpy",
+                "pandas",
+                "matplotlib",
+                "earthpy",
+                "scikit-learn",
+               ]
+
+setup(
+    author="Benjamin Bloss",
+    author_email='bb@emrld.no',
+    # python_requires='>=3.10',
+    description="Geotiff processing utilities",
+    install_requires=requirements,
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='raster_processing',
+    name='disco_tif',
+    packages=find_packages(),
+    url='https://github.com/emerald-geomodelling/disco_tif',
+    version='0.0.1',
+    # zip_safe=False,
+)
