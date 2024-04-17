d5cc63050ac832120a43547b88af049e886f597a
Benjamin R. Bloss <bloss.benjamin@gmail.com>
2024-04-17 10:52:31 +0200

    updated setup.py and cleaned up how the one notebook calls the functions

--- setup.py
+++ setup.py
@@ -3,33 +3,22 @@
 import os,sys
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.md') as history_file:
-    history = history_file.read()
-
-requirements = ["seaborn",
-                "rasterio",
-                "numpy",
-                "pandas",
-                "matplotlib",
-                "earthpy",
-                "scikit-learn",
-               ]
-
-setup(
-    author="Benjamin Bloss",
-    author_email='bb@emrld.no',
-    # python_requires='>=3.10',
-    description="Geotiff processing utilities",
-    install_requires=requirements,
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='raster_processing',
-    name='disco_tif',
-    packages=find_packages(),
-    url='https://github.com/emerald-geomodelling/disco_tif',
-    version='0.0.1',
-    # zip_safe=False,
-)
+setup(name = 'disco_tif',
+      version = "0.0.1",
+      url = 'https://github.com/emerald-geomodelling/disco_tif',
+      author = "Benjamin Bloss",
+      author_email = 'bb@emrld.no',
+      description = "Geotiff processing utilities",
+      install_requires = ["seaborn",
+                          "rasterio",
+                          "numpy",
+                          "pandas",
+                          "matplotlib",
+                          "earthpy",
+                          "scikit-learn",
+                         ],
+      long_description = "Geotiff processing utilities",
+      include_package_data = True,
+      keywords = 'raster_processing',
+      packages = find_packages(),
+     )
